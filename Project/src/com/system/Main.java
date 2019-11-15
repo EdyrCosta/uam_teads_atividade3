68d7b602ccae27f01aadf18c6255a305c6e00849
Edyr Costa <edyrcosta12@gmail.com>
2019-11-15 12:58:47 -0300

    Conclusão do exercício.

--- Project/src/com/system/Main.java
+++ Project/src/com/system/Main.java
@@ -5,7 +5,6 @@ import java.awt.event.*;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.*;
-import javax.xml.transform.Result;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +16,7 @@ public class Main {
 
             window.showLoadingDialog();
                 window.loadRecords();
-                window.modeEdit();
+                window.modeEdit(Window.OPERATION_CREATE);
             window.hideLoadingDialog();
         } catch (IllegalStateException ise) {
             JOptionPane.showMessageDialog(null, "Ocorreu um problema interno. O aplicativo será fechado.",
@@ -59,14 +58,17 @@ public class Main {
         private final String ACTION_NEXT        = "NX";
         private final String ACTION_LAST        = "LT";
 
-        private final byte OPERATION_VIEW       = 0b0;
-        private final byte OPERATION_CREATE     = 0b1;
-        private final byte OPERATION_UPDATE     = 0b10;
+        public static final byte OPERATION_VIEW = 0b0;
+        public static byte OPERATION_CREATE     = 0b1;
+        public static byte OPERATION_UPDATE     = 0b10;
+        public static byte OPERATION_SEARCH     = 0b11;
 
         private byte operation = OPERATION_CREATE;
-
         private ArrayList<Model> clients;
         private int currentClientIndex = -1;
+        private Model lastSearch;
+        private Model searchString;
+        private int currentSearchOccurrence = 0;
 
         public void construct() {
             // Client Frame
@@ -77,7 +79,7 @@ public class Main {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(null);
 
-            // Loading Fram,e
+            // Loading Frame
             frameLoading = new JDialog(frame, "Carregando", true);
             frameLoading.setModal(false);
             frameLoading.setPreferredSize(new Dimension(300 + 16, 112 + 40));
@@ -274,88 +276,178 @@ public class Main {
                         saveRecord();
                         clearFields();
                         loadRecords();
+                        modeEdit(OPERATION_CREATE);
                     }
                     else if (operation == OPERATION_VIEW) {
                         clearFields();
-                        operation = OPERATION_CREATE;
-                        modeEdit();
-                    } else if (operation == OPERATION_UPDATE)
-                        updateRecord();
+                        modeEdit(OPERATION_CREATE);
+                    }
                 } break;
                 case ACTION_DELETE: {
-                    //deleteRecord();
+                    deleteRecord();
                     loadRecords();
-
+                    goToLastRecord();
+                } break;
+                case ACTION_EDIT: {
+                    if (operation == OPERATION_UPDATE) {
+                        saveRecord();
+                        modeView();
+                    } else if (operation == OPERATION_VIEW) {
+                        modeEdit(OPERATION_UPDATE);
+                    }
+                } break;
+                case ACTION_SEARCH: {
+                    if (operation == OPERATION_SEARCH) {
+                        search();
+                    } else {
+                        modeSearch();
+                    }
                 } break;
                 case ACTION_FIRST: {
+                    resetSearch();
                     goToFirstRecord();
                 } break;
                 case ACTION_PREVIOUS: {
+                    resetSearch();
                     goToPreviousRecord();
                 } break;
                 case ACTION_NEXT: {
+                    resetSearch();
                     goToNextRecord();
-                }
+                } break;
                 case ACTION_LAST: {
+                    resetSearch();
                     goToLastRecord();
-                }
+                } break;
                 default: {
                 }
             }
         }
 
-        private void updateRecord() {
+        private void resetSearch() {
+            lastSearch = null;
+            searchString = null;
+            currentSearchOccurrence = 0;
+        }
 
+        private void search() {
+            Model model = new Model();
+            model.setName(txfName.getText());
+            model.setCPF(txfCPF.getText());
+            model.setRG(txfRG.getText());
+            model.setEmail(txfEmail.getText());
+            model.setPhone(txfPhone.getText());
+            model.setCEP(txfCEP.getText());
+
+            if (lastSearch != null) {
+                if (!(model.getName().equals(lastSearch.getName())
+                        && model.getCPF().equals(lastSearch.getCPF())
+                        && model.getRG().equals(lastSearch.getRG())
+                        && model.getEmail().equals(lastSearch.getEmail())
+                        && model.getPhone().equals(lastSearch.getPhone())
+                        && model.getCEP().equals(lastSearch.getCEP()))) {
+                    resetSearch();
+                    searchString = model;
+                }
+            } else {
+                searchString = model;
+            }
+
+            try {
+                model = Database.getInstance().search(searchString, currentSearchOccurrence);
+            } catch (SQLException sqle) {
+                JOptionPane.showMessageDialog(null, "Falha ao pesquisar registro.",
+                        "Erro no banco de dados", JOptionPane.ERROR_MESSAGE);
+            }
+
+            if (model == null) {
+                JOptionPane.showMessageDialog(null, "Não existem registros com os termos pesquisados.",
+                        "Pesquisa", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+
+            currentSearchOccurrence++;
+
+            txfName.setText(model.getName());
+            txfCPF.setText(model.getCPF());
+            txfRG.setText(model.getRG());
+            txfEmail.setText(model.getEmail());
+            txfPhone.setText(model.getPhone());
+            txfCEP.setText(model.getCEP());
+            lastSearch = model;
+        }
+
+        private void deleteRecord() {
+            int reply = JOptionPane.showConfirmDialog(
+                    null,
+                    "Deseja deletar este cliente?",
+                    "Deletar",
+                    JOptionPane.YES_NO_OPTION);
+
+            if (reply == JOptionPane.YES_OPTION) {
+                try {
+                    Database.getInstance().delete(clients.get(currentClientIndex).getId());
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, "Falha ao carregar registro.",
+                            "Erro no banco de dados", JOptionPane.ERROR_MESSAGE);
+                }
+            }
         }
 
         private void goToFirstRecord() {
             modeView();
             currentClientIndex = 0;
             loadRecord(currentClientIndex);
+
             btnFirst.setEnabled(false);
             btnPrevious.setEnabled(false);
+            if (clients.size() >= 2) {
+               btnNext.setEnabled(true);
+               btnLast.setEnabled(true);
+            }
         }
-
         private void goToPreviousRecord() {
             modeView();
             currentClientIndex--;
             loadRecord(currentClientIndex);
+
             if (currentClientIndex == 0) {
                 btnFirst.setEnabled(false);
                 btnPrevious.setEnabled(false);
             }
+            if (clients.size() >= 2 && currentClientIndex < clients.size() - 1) {
+                btnNext.setEnabled(true);
+                btnLast.setEnabled(true);
+            }
         }
-
         private void goToNextRecord() {
             modeView();
             currentClientIndex++;
             loadRecord(currentClientIndex);
-            if (currentClientIndex == clients.size() - 1) {
+
+            if (currentClientIndex == (clients.size() - 1)) {
                 btnNext.setEnabled(false);
                 btnLast.setEnabled(false);
             }
+            if (clients.size() >= 2 && currentClientIndex > 0) {
+                btnPrevious.setEnabled(true);
+                btnFirst.setEnabled(true);
+            }
         }
-
         private void goToLastRecord() {
             modeView();
             currentClientIndex = clients.size() - 1;
             loadRecord(currentClientIndex);
+
+            btnLast.setEnabled(false);
+            btnNext.setEnabled(false);
+            if (clients.size() > 1) {
+                btnPrevious.setEnabled(true);
+                btnFirst.setEnabled(true);
+            }
         }
 
         public void modeView() {
-            lockFields(false);
-            if (clients.size() == 0) {
-                btnAdd.setEnabled(true);
-            } else if (clients.size() == 1) {
-                btnAdd.setEnabled(true);
-                btnEdit.setEnabled(true);
-                btnDelete.setEnabled(true);
-                btnSearch.setEnabled(true);
-                btnFirst.setEnabled(true);
-            } else {
-                lockNavButtons(false);
-            }
-
             operation = OPERATION_VIEW;
 
             txfName.setEditable(false);
@@ -365,27 +457,16 @@ public class Main {
             txfPhone.setEditable(false);
             txfCEP.setEditable(false);
             btnClear.setEnabled(false);
+
+            btnAdd.setEnabled(true);
+            btnDelete.setEnabled(true);
+            btnEdit.setEnabled(true);
         }
-        public void modeEdit() {
+        public void modeEdit(byte mode) {
             lockFields(false);
-            if (clients.size() == 0) {
-                btnAdd.setEnabled(true);
-                btnDelete.setEnabled(false);
-                btnEdit.setEnabled(false);
-                btnSearch.setEnabled(false);
-                lockNavButtons(true);
-            } else if (clients.size() == 1) {
-                btnAdd.setEnabled(true);
-                btnDelete.setEnabled(false);
-                btnEdit.setEnabled(false);
-                btnSearch.setEnabled(true);
-                btnFirst.setEnabled(true);
-                btnPrevious.setEnabled(false);
-                btnNext.setEnabled(false);
-                btnLast.setEnabled(false);
-            } else {
-                lockNavButtons(false);
-            }
+            operation = mode;
+            if (operation == OPERATION_CREATE)
+                currentClientIndex = clients.size();
 
             txfName.setEditable(true);
             txfCPF.setEditable(true);
@@ -393,7 +474,69 @@ public class Main {
             txfEmail.setEditable(true);
             txfPhone.setEditable(true);
             txfCEP.setEditable(true);
+
+            if (mode == OPERATION_CREATE) {
+                btnAdd.setEnabled(true);
+                btnEdit.setEnabled(false);
+            }
+            else if (mode == OPERATION_UPDATE) {
+                btnAdd.setEnabled(false);
+                btnEdit.setEnabled(true);
+            }
             btnClear.setEnabled(true);
+
+            if (clients.size() == 0) {
+                btnDelete.setEnabled(false);
+                btnSearch.setEnabled(false);
+
+                if (mode == OPERATION_UPDATE)
+                    return;
+
+                lockNavButtons(true);
+            } else if (clients.size() == 1) {
+                btnDelete.setEnabled(false);
+                btnSearch.setEnabled(true);
+
+                if (mode == OPERATION_UPDATE)
+                    return;
+
+                btnFirst.setEnabled(true);
+                btnPrevious.setEnabled(false);
+                btnNext.setEnabled(false);
+                btnLast.setEnabled(false);
+            } else {
+                btnDelete.setEnabled(false);
+                btnSearch.setEnabled(true);
+
+                if (mode == OPERATION_UPDATE)
+                    return;
+
+                btnFirst.setEnabled(true);
+                btnPrevious.setEnabled(true);
+                btnNext.setEnabled(false);
+                btnLast.setEnabled(false);
+            }
+        }
+        public void modeSearch() {
+            goToLastRecord();
+
+            operation = OPERATION_SEARCH;
+
+            clearFields();
+            txfName.setEditable(true);
+            txfCPF.setEditable(true);
+            txfRG.setEditable(true);
+            txfEmail.setEditable(true);
+            txfPhone.setEditable(true);
+            txfCEP.setEditable(true);
+            btnClear.setEnabled(true);
+
+            btnAdd.setEnabled(false);
+            btnDelete.setEnabled(false);
+            btnEdit.setEnabled(false);
+            btnSearch.setEnabled(true);
+
+            txfName.requestFocus();
         }
 
         private void loadRecord(int index) {
@@ -419,6 +562,8 @@ public class Main {
             }
 
             Model model = new Model();
+            if (operation == OPERATION_UPDATE)
+                model.setId(clients.get(currentClientIndex).getId());
             model.setName(txfName.getText());
             model.setCPF(txfCPF.getText());
             model.setRG(txfRG.getText());
@@ -427,7 +572,10 @@ public class Main {
             model.setCEP(txfCEP.getText());
 
             try {
-                Database.getInstance().insert(model);
+                if (operation == OPERATION_CREATE)
+                    Database.getInstance().insert(model);
+                else if (operation == OPERATION_UPDATE)
+                    Database.getInstance().update(model);
                 JOptionPane.showMessageDialog(null, "O registro foi salvo com sucesso",
                     "Sucesso", JOptionPane.INFORMATION_MESSAGE);
             } catch (SQLException e) {
@@ -548,6 +696,30 @@ public class Main {
             stmt.executeUpdate();
         }
 
+        public void update(Model model) throws SQLException {
+            String sql = "UPDATE Client SET Name = ?, Cpf = ?, Rg = ?, Email = ?, Phone = ?, Cep = ? WHERE Id = ?";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, model.getName());
+            stmt.setString(2, model.getCPF());
+            stmt.setString(3, model.getRG());
+            stmt.setString(4, model.getEmail());
+            stmt.setString(5, model.getPhone());
+            stmt.setString(6, model.getCEP());
+            stmt.setInt(7, model.getId());
+
+            stmt.executeUpdate();
+        }
+
+        public void delete(int id) throws SQLException {
+            String sql = "DELETE FROM Client WHERE Id = ?";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+
+            stmt.executeUpdate();
+        }
+
         public Model list(int id) throws SQLException {
             String sql = "SELECT Id, Name, Cpf, Rg, Email, Phone, Cep FROM Client WHERE Id = ?";
 
@@ -581,17 +753,42 @@ public class Main {
             while (result.next()) {
                 Model model = new Model();
                 model.setId(result.getInt("Id"));
-//                model.setName(result.getString("Name"));
-//                model.setCPF(result.getString("Cpf"));
-//                model.setRG(result.getString("Rg"));
-//                model.setEmail(result.getString("Email"));
-//                model.setPhone(result.getString("Phone"));
-//                model.setCEP(result.getString("Cep"));
 
                 dados.add(model);
             }
 
             return dados;
+        }
+
+        public Model search(Model model, int currentSearchOccurrence) throws SQLException {
+            String sql = "SELECT Id, Name, Cpf, Rg, Email, Phone, Cep FROM Client " +
+                    "WHERE Name LIKE ? OR Cpf LIKE ? OR Rg LIKE ? OR Email LIKE ? OR Phone LIKE ? OR Cep LIKE ? " +
+                    "LIMIT 1 OFFSET ?";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, model.getName().equals("") ? "null" : "%" + model.getName() + "%");
+            stmt.setString(2, model.getCPF().equals("") ? "null" : "%" + model.getCPF() + "%");
+            stmt.setString(3, model.getRG().equals("") ? "null" : "%" + model.getRG() + "%");
+            stmt.setString(4, model.getEmail().equals("") ? "null" : "%" + model.getEmail() + "%");
+            stmt.setString(5, model.getPhone().equals("") ? "null" : "%" + model.getPhone() + "%");
+            stmt.setString(6, model.getCEP().equals("") ? "null" : "%" + model.getCEP() + "%");
+            stmt.setInt(7, currentSearchOccurrence);
+            ResultSet result = stmt.executeQuery();
+
+            if (result.first()) {
+                Model clientModel = new Model();
+                clientModel.setId(result.getInt("Id"));
+                clientModel.setName(result.getString("Name"));
+                clientModel.setCPF(result.getString("Cpf"));
+                clientModel.setRG(result.getString("Rg"));
+                clientModel.setEmail(result.getString("Email"));
+                clientModel.setPhone(result.getString("Phone"));
+                clientModel.setCEP(result.getString("Cep"));
+
+                return clientModel;
+            }
+
+            return null;
         }
     }
 }
